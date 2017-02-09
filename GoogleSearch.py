da4489110e3b400749f6907022c2a867eeacbc6a
Yashs744 <yash3498@gmail.com>
2017-02-09 13:49:17 +0530

    GoogleSearch

--- GoogleSearch.py
+++ GoogleSearch.py
@@ -1,13 +1,26 @@
-import requests, sys, webbrowser, bs4
+import requests
+import sys
+import webbrowser
+import bs4
+import argparse
 
-print('Googling...')
-res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
-res.raise_for_status()
+parser = argparse.ArgumentParser(description="Search Google and Open's Top 5 Result in Different Tabs")
+parser.add_argument('search', metavar='keyword', type=str, nargs='+',
+			help='Keyword to Search')
 
-soup = bs4.BeautifulSoup(res.text, "lxml")
+args = parser.parse_args()
 
-linkElems = soup.select('.r a')
+def GoogleSearch():
+	base_url = 'http://google.com/search?q='
+	print('Googling...')
 
-numOpen = min(5, len(linkElems))
-for i in range(numOpen):
-	webbrowser.open('http://google.com' + linkElems[i].get('href'))
+	res = requests.get(base_url + ' '.join(sys.argv[1:]))
+	res.raise_for_status()
+
+	soup = bs4.BeautifulSoup(res.text, "lxml")
+
+	linkElems = soup.select('.r a')
+
+	numOpen = min(5, len(linkElems))
+	for i in range(numOpen):
+		webbrowser.open('http://google.com' + linkElems[i].get('href'))
